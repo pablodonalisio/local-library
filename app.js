ba6fd445da790d4ce80ceeb426a1a4c3f2819705
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-09-20 12:03:23 -0300

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require("mongoose");
-var mongoDB =
+var dev_db_url =
   "mongodb+srv://pablo_donalisio:alcr0102@cluster0.lfiwk.mongodb.net/local_library?retryWrites=true&w=majority";
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
